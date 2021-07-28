ccbe64d10165e313d6b8070de5cae453663a2a8d
Tala Alsweiti <talaalsweiti@gmail.com>
2021-07-28 16:45:39 +0300

    Create SOAP API for the project

---
+++ SpringBootRestApp/src/main/java/com/exalt/tasks/springboot/person/GetPersonRequest.java
@@ -0,0 +1,14 @@
+package com.exalt.tasks.springboot.person;
+
+public class GetPersonRequest {
+
+    protected Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ SpringBootRestApp/src/main/java/com/exalt/tasks/springboot/person/GetPersonResponse.java
@@ -0,0 +1,13 @@
+package com.exalt.tasks.springboot.person;
+
+public class GetPersonResponse {
+    protected Person person;
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+}
